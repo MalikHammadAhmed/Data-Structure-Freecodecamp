f4e3deb5c31cec2e735648d2e032cdbda5bc9ef6
Malik Hammad Ahmed <malik_hamamd20@hotmail.com>
2024-06-05 23:02:45 +0100

    Coding Practice files added

---
+++ array.py
@@ -0,0 +1,13 @@
+new_list = [1,2,3]
+#result = new_list[6]
+
+if 1 in new_list : print(True)
+
+for n in new_list:
+    if n == 3:
+        print(True)
+    else:
+        print(False)
+
+numbers = []
+
